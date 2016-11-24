5b69beee5fbfd4d862102cd1dc048e83629b1867
DeathStarGnR <nsbailey2010@gmail.com>
2016-11-23 19:28:30 -0500

    Changes to the OpenShift port information

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.get('/', function (req, res, next) {
   }
 })
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
-})
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+//app.listen(process.env.PORT || 3000, function () {
+//  console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
+//})
